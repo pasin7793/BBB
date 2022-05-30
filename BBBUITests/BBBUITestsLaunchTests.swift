848bf24272233e46cf172c11c30251aacce935b7
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-05-30 20:16:24 +0900

    Initial Commit

---
+++ BBBUITests/BBBUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BBBUITestsLaunchTests.swift
+//  BBBUITests
+//
+//  Created by 임준화 on 2022/05/30.
+//
+
+import XCTest
+
+class BBBUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
