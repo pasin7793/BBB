fa400cf78512d80ccff5adb016d95a23b7551ea9
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-05-30 23:26:05 +0900

    .

---
+++ BBB/Base/BaseVC.swift
@@ -0,0 +1,18 @@
+//
+//  BaseVC.swift
+//  BBB
+//
+//  Created by 임준화 on 2022/05/30.
+//
+
+import Foundation
+import UIKit
+
+final class BaseVC: UIViewController{
+    
+    override func viewDidLoad() {
+        <#code#>
+    }
+    func setUp(){}
+    func 
+}
